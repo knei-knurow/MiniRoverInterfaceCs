5c646fd0d8fb73e26ce8ba4025a63dcf00cafe15
MichalZywerKNEI <MichalZywerKNEI@gmail.com>
2023-01-03 20:28:26 +0100

    Add project files.

---
+++ MiniRoverInterfaceCs/Interface.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace MiniRoverInterfaceCs
+{
+    public partial class Interface : Form
+    {
+        public string ProgramVersion = @"0.0.0.2";
+        //public Thread SendDataThread;
+        public Thread RecieveDataThread;
+
+        public Interface()
+        {
+            InitializeComponent();
+        }
+
+        private void Interface_Load(object sender, EventArgs e)
+        {
+            this.Text = @"MiniRover Interface Version " + ProgramVersion;
+            BtnEdit.Enabled = true;
+            BtnConnect.Enabled = true;
+            BtnSave.Enabled = false;
+            BtnDisconnect.Enabled = false;
+            TxtSendPort.Enabled = false;
+            TxtRecievePort.Enabled = false;
+        }
+
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            BtnConnect.Enabled = false;
+            BtnSave.Enabled = true;
+            TxtSendPort.Enabled = true;
+            TxtRecievePort.Enabled = true;
+
+            BtnEdit.Enabled = false;
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+            if ((TxtSendPort.Text != TxtRecievePort.Text) && TxtSendPort.Text != @"" && TxtRecievePort.Text != @"")
+            {
+                BtnEdit.Enabled = true;
+                BtnConnect.Enabled = true;
+                TxtSendPort.Enabled = false;
+                TxtRecievePort.Enabled = false;
+
+                BtnSave.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show(@"Select different and not empty Send and Recieve Ports.");
+            }
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            if (TxtSendPort.Text != @"" && TxtRecievePort.Text != @"")
+            {
+                BtnDisconnect.Enabled = true;
+                BtnEdit.Enabled = false;
+
+                SerialPort1.PortName = TxtSendPort.Text;
+                SerialPort2.PortName = TxtRecievePort.Text;
+
+                SerialPort1.Open();
+                SerialPort2.Open();
+
+
+                BtnConnect.Enabled = false;
+            }
+        }
+
+        private void BtnDisconnect_Click(object sender, EventArgs e)
+        {
+            if (SerialPort1.IsOpen || SerialPort2.IsOpen)
+            {
+                SerialPort1.Close();
+                SerialPort2.Close();
+                BtnConnect.Enabled = true;
+                BtnEdit.Enabled = true;
+
+                BtnDisconnect.Enabled = false;
+            }
+        }
+
+        private void TxtSendPort_DropDown(object sender, EventArgs e)
+        {
+            TxtSendPort.Items.Clear();
+            string[] COMPorts = SerialPort.GetPortNames();
+            foreach (string comport in COMPorts)
+            {
+                TxtSendPort.Items.Add(comport);
+            }
+        }
+
+        private void TxtRecievePort_DropDown(object sender, EventArgs e)
+        {
+            TxtRecievePort.Items.Clear();
+            string[] COMPorts = SerialPort.GetPortNames();
+            foreach (string comport in COMPorts)
+            {
+                TxtRecievePort.Items.Add(comport);
+            }
+        }
+    }
+}
